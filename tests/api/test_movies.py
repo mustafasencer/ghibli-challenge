99e74004b685a71faa51653d994bd82c3f039cf3
mustafasencerozcan <rumansare12345>
2020-10-11 20:59:18 +0300

    429 test added

--- tests/api/test_movies.py
+++ tests/api/test_movies.py
@@ -6,6 +6,12 @@ def test_list_movies(client):
     assert response.status == '200 OK'
 
 
+def test_list_movies_429(client):
+    for _ in range(31):
+        response = client.get('/movies')
+    assert response.status == '429 TOO MANY REQUESTS'
+
+
 def test_get_movie(client):
     response = requests.get(f'{client.application.config["GHIBLI_URL"]}/films').json()
     assert isinstance(response, list)
